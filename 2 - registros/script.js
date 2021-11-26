9c5b910ab212994ae723e37b8191aca2257e9aef
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-26 18:19:26 -0300

    Aula 2

---
+++ 2 - registros/script.js
@@ -0,0 +1,11 @@
+// declaração do tipo de dado
+
+var estrutura_livro = { 
+    nome: "As aventuras de Tintim",
+    preco: "20.00",
+    pagina: 100
+}
+
+// declaração das variáveis
+
+console.log(`O nome do livro é: ${estrutura_livro.nome}, possui ${estrutura_livro.pagina} páginas e custa R$${estrutura_livro.preco}`);
