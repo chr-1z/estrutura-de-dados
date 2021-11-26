f46755234840f4f57d401c722685ee2783d02380
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-26 17:31:56 -0300

    Aula 1

---
+++ 1/script.js
@@ -0,0 +1,48 @@
+// function inicio() {
+
+//     vetor = ['1', '3', '5', '7', '9']; // cria o vetor com valores pré-definidos
+
+//     achou = false; // Variável para armazenar o resultado da procura
+
+//     numero = Number(prompt("Qual número deseja procurar? "));
+
+//     for (let posicao = 0; posicao < 5; posicao++) {
+
+//         if (vetor[posicao] == numero) {
+
+//             console.log("Encontrado na posição: ", posicao);
+//             achou = true;
+
+//         } else if (!achou) {
+
+//             console.log("O número não está no vetor!");
+
+//         }
+//     }
+// }
+
+// inicio();
+
+// #################################################################
+
+// function inicio() {
+
+//     // Criação dos vetores, já com os dados inicializados
+//     nome = ["André", "Thiago", "Bruno", "Carlos", "Cassio"];
+//     altura = [1.71, 1.78, 1.75, 1.87, 1.71];
+
+//     console.log("---------------------");
+//     console.log("------ TABELA ------ ");
+//     console.log("---------------------");
+
+//     for (let posicao = 0; posicao < 5; posicao++) {
+
+//         console.log(nome[posicao], + "  " + altura [posicao]);
+
+//     }
+
+// }
+
+// inicio()
+
+// #################################################################
