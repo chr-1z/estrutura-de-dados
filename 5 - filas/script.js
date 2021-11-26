9b448dd1685049291a91ff5ea322a35cb67d698d
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-26 19:03:27 -0300

    Aula 5

---
+++ 5 - filas/script.js
@@ -0,0 +1,13 @@
+/* 
+    A estrutura do tipo Fila admite remoção de elementos
+    e inserção de novos sujeita à seguinte regra de operação:
+
+    O elemento removido é o que está na estrutura há mais tempo,
+    ou seja, o primeiro objeto inserido na fila é também o primeiro
+    a ser removido seguindo o conceito 
+    FIFO - First In First Out - Primeiro a Entrar Primeiro a Sair
+
+
+    Insere e remove
+*/
+
