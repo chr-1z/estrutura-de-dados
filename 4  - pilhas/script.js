c7f4e4d99f1cd3633a835d58bc95e09f33874356
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-26 18:58:59 -0300

    Aula 4

---
+++ 4  - pilhas/script.js
@@ -0,0 +1,27 @@
+// Pilhas
+
+/*
+    São uma estrutura de dados que serve como 
+    uma coleção de elementos, e permite o acesso 
+    a somente um item de dados armazenado.
+
+    O acesso aos itens de uma pilha é restrito
+    Somente um item pode ser lido ou removido por vez.
+*/
+
+/* 
+    Tipos de Pilhas:
+
+    LIFO ou UEPS
+    Last In First Out - Último que Entra Primeiro que Sai
+    O primeiro elemento a ser retirado é o último que tiver sido inserido
+
+    Empty Stack - Push - Pop
+    Pilha vazia - Inserir - Remover
+
+    FIFO ou PEPS
+    First In First Out - Primeiro que Entra Primeiro que Sai
+    O primeiro que tiver sido inserido
+    Enqueue - Dequeue
+    Insere da fila - Retira da fila
+*/
