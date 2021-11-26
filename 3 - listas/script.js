fea0b456b489e32a63fe006041f27a019a3dcffc
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-26 18:49:00 -0300

    Aula 3

---
+++ 3 - listas/script.js
@@ -0,0 +1,17 @@
+// listas - tamanho ajustável
+// array - tamanho fixo
+
+// existem 2 tipos de listas:
+
+// ligadas
+/* 
+    Existem os nós onde cada um dos nós conhece o valor 
+    que está sendo armazenado em seu interior além de conhecer 
+    o elemento posterior a ele: por isso ela é chamada de "lista ligada", 
+    pois os nós são amarrados com essa indicação de qual é o próximo nó.
+*/
+
+// duplamente ligadas
+/* 
+    São bidirecionais, podem ir ao valor anterior e próximo
+*/
