7f4a458255ee370325dae7289e418f801b13f2a9
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-27 11:36:02 -0300

    Aula 8

---
+++ 8 - grafos/script.js
@@ -0,0 +1,11 @@
+/*
+    GRAFOS
+
+    Grafos são estruturas que permitem programar a relação entre objetos
+
+    Os Objetos são vértices ou "nós" do grafo
+
+    Os relacionamentos são arestas
+
+
+*/
