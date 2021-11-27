8db4867689c78c947a808f4acef45dd9aa81027c
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-27 11:32:16 -0300

    Aula 7

---
+++ 7 - tabela hash/script.js
@@ -0,0 +1,33 @@
+/*
+
+    #Tabela de Espalhamento
+
+    Uma tabela hash, de dispersão ou espalhamento é uma
+    estrutura de dados especial, que associa chaves de 
+    pesquisa a valores.
+
+
+
+    #Hashing
+
+    Uma tabela hash é uma generalização da idéia de array,
+    porém utiliza uma função denominada Hashing para 
+    espalhar os elementos, fazendo com que os mesmo fiquem
+    de forma não ordenada dentro do "array" que define
+    a tabela.
+
+    A tabela hash permite a associação de "valores" a "chaves"
+
+    Valores: é a posição ou índice onde o elemento se encontra
+
+    Chave: parte da informação que compõe o elemento a ser
+    manipulado.
+
+    Espalhar facilita a busca na estrutura de dados, pois a
+    partir de uma chave podemos acessar de forma rápida uma
+    posição do "array".
+
+
+
+
+*/
